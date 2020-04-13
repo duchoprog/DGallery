83669e9e4a47382b83be62d4c981cef4e7d69319
Pablo Duchovny <pablo.duchovny@gmail.com>
2020-04-13 20:12:49 -0300

    removed mail form

--- public/src/galeria2.js
+++ public/src/galeria2.js
@@ -4,44 +4,45 @@ let botArtist = document.getElementById("artistBot");
 let botContact = document.getElementById("contactBot");
 let botHamb = document.getElementById("hamb");
 let modal = document.getElementById("myModal");
-vergaleria()
+vergaleria();
 
 function vergaleria() {
-
-  firebase.database().ref("/obras").on('value', function (snapshot) {
-    document.querySelector(".carousel-inner").innerHTML = ""
-    document.querySelector(".carousel-indicators").innerHTML = ""
-    var i = 0
-    snapshot.forEach(function (child) {
-
-      armarTarjetaDB(child.val().nombre, child.val().comentarios, child.val().obraURL, child.val().borrador, i)
-      i++
-
-
-
+  firebase
+    .database()
+    .ref("/obras")
+    .on("value", function (snapshot) {
+      document.querySelector(".carousel-inner").innerHTML = "";
+      document.querySelector(".carousel-indicators").innerHTML = "";
+      var i = 0;
+      snapshot.forEach(function (child) {
+        armarTarjetaDB(
+          child.val().nombre,
+          child.val().comentarios,
+          child.val().obraURL,
+          child.val().borrador,
+          i
+        );
+        i++;
+      });
     });
-  })
 }
 
 function armarTarjetaDB(nombre, comentarios, url, borrador, counter) {
-
-  var counter = counter
-  console.log(counter + " " + url)
-  var activator = ""
-  if (counter == 0) { activator = "active" }
+  var counter = counter;
+  console.log(counter + " " + url);
+  var activator = "";
+  if (counter == 0) {
+    activator = "active";
+  }
   var tarj = `<div class="item ${activator}" style="height:75vh;width:90vw; ">
-  <img src=${url}  class="center "   /></div>`
-  document.querySelector(".carousel-inner").innerHTML += tarj
+  <img src=${url}  class="center "   /></div>`;
+  document.querySelector(".carousel-inner").innerHTML += tarj;
   //var popo = document.createElement("div").innerHTML()
 
   //document.querySelector("body").appendChild(popo)
-  var punto = `<li data-target="#myCarousel" data-slide-to="${counter}" class=" ${activator}"></li>`
-  document.querySelector(".carousel-indicators").innerHTML += punto
-
+  var punto = `<li data-target="#myCarousel" data-slide-to="${counter}" class=" ${activator}"></li>`;
+  document.querySelector(".carousel-indicators").innerHTML += punto;
 }
-
-
-
 
 botArtist.addEventListener("click", function (event) {
   event.preventDefault();
